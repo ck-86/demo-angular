f7914f61185cf30389d32af84b3e9551035e8314
CK <ck86@gmx.com>
2014-06-26 13:20:26 +0530

    Nested Controllers Example

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,15 @@
 var app = angular.module('myApp',[]);
+/*----------------------------------------/
+	Creating Controller Nesting
+/-----------------------------------------*/	
 
-app.controller('FirstController' , function($scope){
-	$scope.message = "Hello Angular"
+app.controller('ParentController', function($scope){
+	$scope.person = { greeted : false };
+});
+
+app.controller('ChildController', function($scope){
+	$scope.sayHello = function() {
+		$scope.person.name = "Chetan";
+		$scope.person.greeted = true;
+	}
 });
