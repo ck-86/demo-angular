ca1ed6c2a24f4aabcdd5d421545092b9fd955fe2
CK <ck86@gmx.com>
2014-06-26 18:57:45 +0530

    Used Parse Service of Angular

--- client/js/app.js
+++ client/js/app.js
@@ -1,15 +1,20 @@
-var app = angular.module('myApp',[]);
-/*----------------------------------------/
-	Creating Controller Nesting
-/-----------------------------------------*/	
+    // open this example and type person.name into the test field
+var App = angular.module('myApp', [])
 
-app.controller('ParentController', function($scope){
-	$scope.person = { greeted : false };
-});
+App.controller('MyController',
+    ['$scope', '$parse', function($scope, $parse) {
 
-app.controller('ChildController', function($scope){
-	$scope.sayHello = function() {
-		$scope.person.name = "Chetan";
-		$scope.person.greeted = true;
-	}
-});
+      $scope.person = {
+        name: "Bruce Wayne",
+        city: 'Gotham'
+      };
+
+      $scope.$watch('expr', function(newVal, oldVal, scope) {
+        if (newVal !== oldVal) {
+          // Let's set up our parseFun with the expression
+          var parseFun = $parse(newVal);
+          // Get the value of the parsed expression, set it on the scope for output
+          scope.parsedExpr = parseFun(scope);
+        }
+      });
+    }]);
