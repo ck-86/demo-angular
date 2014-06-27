8e786b5280c9c0a5f38d0ae761f5d743d860caa2
CK <ck86@gmx.com>
2014-06-27 15:31:31 +0530

    Interpolation Completed

--- client/js/app.js
+++ client/js/app.js
@@ -1,20 +1,34 @@
-    // open this example and type person.name into the test field
-var App = angular.module('myApp', [])
+angular.module('emailParser', [])
+.config(['$interpolateProvider',
+function($interpolateProvider) {
+$interpolateProvider.startSymbol('__');
+$interpolateProvider.endSymbol('__');
+}])
+.factory('EmailParser', ['$interpolate',
+function($interpolate) {
+// a service to handle parsing
+return {
+parse: function(text, context) {
+var template = $interpolate(text);
+return template(context);
+}
+}
+}]);
+
+
+// open this example and type person.name into the test field
+var App = angular.module('myApp', ['emailParser']);
 
 App.controller('MyController',
-    ['$scope', '$parse', function($scope, $parse) {
-
-      $scope.person = {
-        name: "Bruce Wayne",
-        city: 'Gotham'
-      };
-
-      $scope.$watch('expr', function(newVal, oldVal, scope) {
-        if (newVal !== oldVal) {
-          // Let's set up our parseFun with the expression
-          var parseFun = $parse(newVal);
-          // Get the value of the parsed expression, set it on the scope for output
-          scope.parsedExpr = parseFun(scope);
-        }
-      });
-    }]);
+['$scope', 'EmailParser',
+function($scope, EmailParser) {
+// Set up a watch
+$scope.$watch('emailBody', function(body) {
+if (body) {
+$scope.previewText =
+EmailParser.parse(body, {
+to: $scope.to
+});
+}
+});
+}]);
