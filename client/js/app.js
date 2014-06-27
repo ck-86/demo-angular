eb50923c9deb7a8da8f4f09a69acf247b2cda736
CK <ck86@gmx.com>
2014-06-27 19:03:17 +0530

    Testing Filters

--- client/js/app.js
+++ client/js/app.js
@@ -1,34 +1,10 @@
-angular.module('emailParser', [])
-.config(['$interpolateProvider',
-function($interpolateProvider) {
-$interpolateProvider.startSymbol('__');
-$interpolateProvider.endSymbol('__');
-}])
-.factory('EmailParser', ['$interpolate',
-function($interpolate) {
-// a service to handle parsing
-return {
-parse: function(text, context) {
-var template = $interpolate(text);
-return template(context);
-}
-}
-}]);
-
-
 // open this example and type person.name into the test field
-var App = angular.module('myApp', ['emailParser']);
+var App = angular.module('myApp', []);
 
-App.controller('MyController',
-['$scope', 'EmailParser',
-function($scope, EmailParser) {
-// Set up a watch
-$scope.$watch('emailBody', function(body) {
-if (body) {
-$scope.previewText =
-EmailParser.parse(body, {
-to: $scope.to
-});
-}
-});
-}]);
+App.controller('MyController', ['$scope', '$filter' , function($scope, $filter){
+
+  $scope.name = $filter('lowercase')('CHETAN');
+
+  $scope.today = new Date();
+  
+}])
